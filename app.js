14ba3cfb2dca9289c1c20947e11d80f866c893de
Zerlane <nautishayc@gmail.com>
2023-08-27 12:55:04 -0500

    logic is there, content might be wrong

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Arrived");
-})
+//https://webtips.dev/tic-tac-toe-in-javascript
 
 let gameEnded = false;
-let boxes = document.getElementsByClassName("box");
+let playerX = {
+    str: 'X',
+    cells: [],
+};
+let playerO = {
+    str: 'O',
+    cells: [],
+};
+let currentPlayer = playerX;
 let winCombos = [
     //rows
-    ['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'],
+    ['0', '1', '2'], ['3', '4', '5'], ['6', '7', '8'],
 
     //diagonal
-    ['1', '5', '9'], ['3', '5', '7'],
+    ['0', '4', '8'], ['2', '4', '6'],
 
     //columns
-    ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9']
+    ['0', '3', '6'], ['1', '4', '7'], ['2', '5', '8']
 ]
 
+document.addEventListener("click", event => {
+    let target = event.target;
+    let data_val = target.getAttribute('data-value');
 
+    if (target.textContent.length == 0) { //Change later, might not be the smartest way
+        currentPlayer.cells.push(data_val);
+        target.innerText = currentPlayer.str;
+    } else {
+        console.log("Occupied");
+    }
 
+    if (playerX) {
+        return currentPlayer = playerO;
+    }
+    if (playerO) {
+        return currentPlayer = playerX;
+    }
 
-
-console.log(boxes)
+});
