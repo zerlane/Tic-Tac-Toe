589366f85f6626dbb3600d9424923b98ae9f9619
Zerlane <nautishayc@gmail.com>
2023-08-28 18:04:30 -0500

    switched players, working on checking winner

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ let playerO = {
     str: 'O',
     cells: [],
 };
-let currentPlayer = playerX;
+let currentPlayer;
+let xTurn = true;
+
 let winCombos = [
     //rows
     ['0', '1', '2'], ['3', '4', '5'], ['6', '7', '8'],
@@ -21,22 +23,48 @@ let winCombos = [
     ['0', '3', '6'], ['1', '4', '7'], ['2', '5', '8']
 ]
 
-document.addEventListener("click", event => {
+let board = document.getElementById('board');
+
+board.addEventListener("click", event => {
     let target = event.target;
     let data_val = target.getAttribute('data-value');
 
-    if (target.textContent.length == 0) { //Change later, might not be the smartest way
-        currentPlayer.cells.push(data_val);
-        target.innerText = currentPlayer.str;
+    if (xTurn) {
+        currentPlayer = playerX;
+        xTurn = false;
     } else {
-        console.log("Occupied");
+        currentPlayer = playerO;
+        xTurn = true;
     }
 
-    if (playerX) {
-        return currentPlayer = playerO;
-    }
-    if (playerO) {
-        return currentPlayer = playerX;
-    }
+    currentPlayer.cells.push(data_val);
 
+    //check winner here then move to own function
+    // if (currentPlayer.cells.length == 3) {
+    //     winCombos.forEach(arr => {
+    //         if (arr == currentPlayer.cells) {
+    //             console.log(`${currentPlayer.str} won.`)
+    //         }
+    //     })
+    // }
+
+    console.log(target);
+    target.innerHTML = `<p>${currentPlayer.str}</p>`    
 });
+
+let fillBox = () => {
+   
+}
+
+let switchPlayers = () => {
+   
+};
+
+let checkWinner = () => {
+
+    //determine if draw
+};
+
+let resetGame = () => {
+
+}
